6849638db9cd75001ec5c8275aad17e22f8b8268
Amit.satpute <asatpute@appnexus.com>
2020-10-03 16:06:08 +0530

    Added Classes and Interface

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime TimeStamp { get; protected set; }
+
+        protected Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+
+
+    }
+}
